442d60fe41db7362abe28a53e35158886648127a
nrdmraju <neilrd01@gmail.com>
2021-05-01 22:53:08 -0500

    re-created the tables and added the primary and foreign keys.

--- project1-sql-script.sql
+++ project1-sql-script.sql
@@ -21,10 +21,6 @@ type_description VARCHAR(15) NOT NULL UNIQUE
 );
 
 
-
-
-
-
 DROP TABLE users;
 
 CREATE TABLE users (
@@ -34,6 +30,18 @@ CREATE TABLE users (
 	first_name VARCHAR(25) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
 	email VARCHAR(35) NOT NULL,
-	user_role VARCHAR(20)
+	user_role integer REFERENCES roles(role_id)
+	
+);
+
+DROP TABLE account;
+DROP TABLE accounts;
+
+CREATE TABLE accounts(
+ accountId INTEGER PRIMARY KEY, 
+ balance INTEGER NOT NULL,
+ account_status INTEGER REFERENCES account_status(status_id),
+ account_type INTEGER REFERENCES account_types(type_id),
+ user_id INTEGER REFERENCES users(user_id)
 );
 
